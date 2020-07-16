b70629ba02e89c524dbb4b8a5f13b129f9f1e1c8
954218947@qq.com <123yongchun>
2020-07-16 22:50:52 +0800

    Initial commit

---
+++ desktop/src/org/tj/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.tj.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.tj.game.MyCorpseGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyCorpseGame(), config);
+	}
+}
